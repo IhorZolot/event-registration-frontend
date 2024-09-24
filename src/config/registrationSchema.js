c425e0d9619a12b5b330c21aaacafc4f222a2e58
IhorZolot <fedonn@ukr.net>
2024-09-24 20:23:30 +0300

    update schems registration

--- src/config/registrationSchema.js
+++ src/config/registrationSchema.js
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 
+const today = new Date();
+const eighteenYearsAgo = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+
 const registrationSchema = z.object({
   fullName: z.string().min(3, 'Name must be at least 3 characters long'),
   email: z.string().email(),
-  dateOfBirth: z.date({ required_error: 'Date of birth is required' }).min(18, 'You must be at least 18 years old'),
+  dateOfBirth: z.date({ required_error: 'Date of birth is required' }).max(eighteenYearsAgo, 'You must be at least 18 years old'),
   source: z.string().min(1, { message: 'Source is required' }),
 });
 
